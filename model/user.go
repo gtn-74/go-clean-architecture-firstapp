2149eabef16d3e306ba7e4079dcced32434e5677
gtn-74 <gtn.r74@gmail.com>
2024-12-31 17:55:43 +0900

    wip:create model

---
+++ model/user.go
@@ -0,0 +1,25 @@
+// user.goがmodelのパッケージに所属してることを明示する必要があるらしい
+package model
+
+import "time"
+
+/*
+* 下の型をjson形式に変換したタイミングで、""で囲っている文字列をプロパティに置換してくれる
+*  `json:"id" gorm:"primaryKey"`
+ */
+
+type User struct {
+	ID       uint      `json:"id" gorm:"primaryKey"`
+	Email    string    `json:"email" gome:"unique"`
+	Password string    `json:"password"`
+	CreateAt time.Time `json:"create_at"` // goのtimeパッケージのtime型を採用
+	UpdateAt time.Time `json:"update_at"`
+}
+
+// サインアップのエンドポイント
+// クライアントにレスポンスで返すデータの型
+
+type UserResponse struct {
+	ID    uint   `json:"id" gorm:"primaryKey"`
+	Email string `json:"email" gorm:"unique"`
+}
