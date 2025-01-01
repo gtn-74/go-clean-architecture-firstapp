d241c7cc84a92de9bfda5aebc0d6d14ce90db1e4
gtn-74 <gtn.r74@gmail.com>
2025-01-01 12:41:35 +0900

    create: database

---
+++ db/db.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	// godotenvは、外部パッケージのため、`go mod tidy`コマンドでインストールしておく必要がある
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() *gorm.DB {
+	// ここにDBの初期化処理を書く
+	// 環境変数を読み込むための処理
+	if os.Getenv("GO_ENV") == "dev" {
+		err := godotenv.Load() // .envファイルを読み込む
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	// POSTGRES_USER, POSTGRES_PW, POSTGRES_HOST, POSTGRES_PORT, POSTGRES_DBは、.envファイルに書いてる。
+	url := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PW"), os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"))
+
+	// urlとdbを接続
+	db, err := gorm.Open((postgres.Open(url)), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(("Connceted")) // 接続
+	return db
+}
+// gormは、外部パッケージのため、`go mod tidy`コマンドでインストールしておく必要がある
+
+func CloseDB(db *gorm.DB) {
+	sqlDB, _ := db.DB()
+	if err := sqlDB.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
--- model/task.go
+++ model/task.go
@@ -10,3 +10,13 @@ type Task struct {
 	User     User      `json:"user" gorm:"foreignKey:UserId; constraint: OnDelete:CASCADE"`
 	UserId   uint      `json:"user_id" gorm:"not null"`
 }
+// Userのprimary keyがUserIdと紐づくことで1対多の関係にできる ID       uint      `json:"id" gorm:"primaryKey"`
+
+// constraint: OnDelete:CASCADE でUserが削除されたらTaskも削除される
+
+type TaskResponse struct {
+	ID       uint      `json:"id" gorm:"primaryKey"`
+	Title    string    `json:"title" gorm:"not null"`
+	CreateAt time.Time `json:"create_at"`
+	UpdateAt time.Time `json:"update_at"`
+}
