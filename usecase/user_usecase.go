4cfea4ae435b8c42e05aad47ff2ef95fdd79137c
gtn-74 <gtn.r74@gmail.com>
2025-01-04 22:02:03 +0900

    feat:User usecase

---
+++ usecase/user_usecase.go
@@ -0,0 +1,72 @@
+package usecase
+
+import (
+	"go-clean-architecture-firstapp/model"
+	"go-clean-architecture-firstapp/repository"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// user_usecaseに必要な、メソッドが、
+// signUpとsignInの2である理由がわからない
+type IUserUsecase interface {
+	SignUp(user model.User) (model.UserResponse, error) // signUpの返り値
+	Login(user model.User) (string, error)              // Loginの返り値
+}
+
+// ! urがよくわからない
+type userUsecase struct {
+	// urは、フィールド
+	ur repository.IUserRepository
+}
+
+func NewUserUsecase(ur repository.IUserRepository) IUserUsecase {
+	return &userUsecase{ur}
+}
+
+// signUPメソッド
+func (uu *userUsecase) SignUp(user model.User) (model.UserResponse, error) {
+	// signupの引数であるuserには、クライアントがわから渡ってきた入力データが入る
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), 10)
+	// byte(user.Password)をハッシュ化
+	if err != nil {
+		return model.UserResponse{}, err
+		// 返値では、UserResponseの後ろに空{}を記述しないといけない
+	}
+	// user.Passwordで渡ってきた文字列をbcryptで暗号化して、newUserのハッシュ化
+	newUser := model.User{Email: user.Email, Password: string(hash)}
+	// ! := は、変数宣言と代入を同時に行う。つまり var hoge = fuga と同じ
+	// var newUser = model.User{Email: user.Email, Password: string(hash)}
+
+	// CreateUserを呼美出す
+	if err := uu.ur.CreateUser(&newUser); err != nil {
+		return model.UserResponse{}, err
+	}
+	// user_repositoryで、レスポンスを構造として持ってる。返却値に値を代入してる
+	resUser := model.UserResponse{
+		ID:    newUser.ID,
+		Email: newUser.Email,
+	}
+	return resUser, nil
+}
+
+func (uu *userUsecase) Login(user model.User) (string, error) {
+	storedUser := model.User{}
+	// db側で持ってる情報と入力された情報の照らし合わせ
+	if err := uu.ur.GetUserByEmail(&storedUser, user.Email); err != nil {
+		return "", err
+	}
+	// パスワードが一致したらjwtトークンを生成
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"user_id": storedUser.ID,
+		"exp":     time.Now().Add(time.Hour * 12).Unix(), // アクセストークン時間
+	})
+	tokenString, err := token.SignedString(([]byte(os.Getenv("SECRET")))) // SignedStringでjwtトークンを生成できる
+	if err != nil {
+		return "", err
+	}
+	return tokenString, nil
+}
